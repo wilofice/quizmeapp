2471aa5c913d14d26bc4a1960334f053d52a9b96
Genereux Alahassa <wilofice@gmail.com>
2017-06-16 02:04:24 +0000

    Add project files.

---
+++ QuizMeApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizMeApp.Startup))]
+namespace QuizMeApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
